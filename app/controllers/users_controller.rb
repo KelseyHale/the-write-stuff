de74ba947784be467cdecb9b73ebc0ff2d3da91e
Kelsey Hale <kelseyhale@Kelseys-MacBook-Pro.local>
2016-02-20 09:03:11 -0600

    remove old code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,6 @@ class UsersController < ApplicationController
     @questions = Question.all
     @question = Question.generate_random_unanswered_question(current_user)
     @answer = Answer.new
-    # @question = @current_question
     @answers = Answer.where(question: @current_question)
-    # binding.pry
-    # @question = Question.find(params[:id])
   end
 end
