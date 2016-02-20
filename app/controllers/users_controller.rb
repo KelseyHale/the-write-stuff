f052965e0ed4cee8ef850ae445013792a881d7aa
Kelsey Hale <kelseyhale@Kelseys-MacBook-Pro.local>
2016-02-20 09:10:03 -0600

    only show previously answered questions on dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @questions = Question.all
+    @previous_answered_questions = current_user.questions
     @question = Question.generate_random_unanswered_question(current_user)
     @answer = Answer.new
     @answers = Answer.where(question: @current_question)
